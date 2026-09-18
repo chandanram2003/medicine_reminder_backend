5dba96b641d9a2d3e9a1bcc7698f2845813c1aa6
Chandan  ram <ck660405@gmail.com>
2026-09-18 16:43:38 +0530

    Update medicineController.js

--- controllers/medicineController.js
+++ controllers/medicineController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 const Medicine = require("../models/medicine")
 const { sendReminderSms } = require("../utils/sendSms");
 // ADD MULTIPLE MEDICINES
